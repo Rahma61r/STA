3af747c7bb79c473f32988e4af69a40db35e4962
Rahma61r <rahma5002ramadan@gmail.com>
2025-05-10 19:14:58 +0300

    00

---
+++ Assets/BallCollector.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class BallCollector : MonoBehaviour
+{
+    public TextMeshProUGUI counterText; // بدل Text
+    public int maxBalls = 9;
+    private int ballCount = 0;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Ball") && ballCount < maxBalls)
+        {
+            ballCount++;
+
+            // عداد الكرات
+            counterText.text = ballCount.ToString();
+
+            // تغيير لون الكرة
+            HighlightBall(other.gameObject);
+
+            // منع احتساب الكرة مرتين
+            other.tag = "Untagged";
+        }
+    }
+
+    void HighlightBall(GameObject ball)
+    {
+        Renderer rend = ball.GetComponent<Renderer>();
+        if (rend != null)
+        {
+            rend.material.color = Color.yellow; // مثال: يخلي الكرة تصفر
+        }
+    }
+}
