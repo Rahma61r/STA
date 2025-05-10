33f6a5b7457ac4dd16e2ac1514f5de83677c179e
1SaadMohamed1 <abogabalsaad23@gmail.com>
2025-05-10 17:56:39 +0300

    saad
    arrows

---
+++ Assets/Code/SantaCardUI.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class SantaCardUI : MonoBehaviour
+{
+    [SerializeField] private GameObject[] arrows;
+    [SerializeField] private GameObject cardPanel;
+
+
+    public void OnBuyButtonClicked()
+    {
+        // أخفي الكارد
+        cardPanel.SetActive(false);
+
+        // أظهر الأسهم
+        foreach (GameObject arrow in arrows)
+        {
+            arrow.SetActive(true);
+        }
+
+        // خصم الكوينز لو عندك نظام عملات (اختياري)
+        // PlayerCoins.Instance.Deduct(10);
+    }
+}
+
---
+++ Assets/Code/chathree.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class chathree : MonoBehaviour
+{
+    [SerializeField] private GameObject[] arrows;
+    [SerializeField] private GameObject cardPanel;
+
+
+    public void OnBuyButtonClicked()
+    {
+        // أخفي الكارد
+        cardPanel.SetActive(false);
+
+        // أظهر الأسهم
+        foreach (GameObject arrow in arrows)
+        {
+            arrow.SetActive(true);
+        }
+
+        // خصم الكوينز لو عندك نظام عملات (اختياري)
+        // PlayerCoins.Instance.Deduct(10);
+    }
+}
